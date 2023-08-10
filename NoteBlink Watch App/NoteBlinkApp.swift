0c198249afba702f7a0f92a705a69fb46c8a25a7
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-08-10 11:53:15 +0200

    Initial Commit

---
+++ NoteBlink Watch App/NoteBlinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteBlinkApp.swift
+//  NoteBlink Watch App
+//
+//  Created by Cristina on 2023-08-10.
+//
+
+import SwiftUI
+
+@main
+struct NoteBlink_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
